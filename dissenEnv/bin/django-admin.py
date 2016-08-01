f4b31910b1fdb1ec59a7f32fbabfaf588a3c3e8f
kennedal <kennedal@kth.se>
2016-08-01 17:06:48 +0200

    initial commit

---
+++ dissenEnv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/axelkennedal/Dropbox/Programmering/D-Dagen/Dissen/dissenEnv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
