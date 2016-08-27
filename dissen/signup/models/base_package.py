2cc49025e9c22e71fc46017b2d8448f54b221441
tbrodd <tbrodd@kth.se>
2016-08-27 17:28:05 +0200

    added a base package

---
+++ dissen/signup/models/base_package.py
@@ -0,0 +1,11 @@
+class BasePackage(Package):
+    """Describes the base package."""
+
+    PEOPLE_DAY = 2
+    PEOPLE_EVENING = 2
+    DRINK_TICKETS = 10
+    TABLES = 1
+
+    def __init__(self):
+        self.PRICE = 2500
+        self.DESCRIPTION = ""
