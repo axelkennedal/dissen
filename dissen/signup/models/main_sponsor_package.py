85c979958e5be5c78ffa204016beedde7ef497cf
tbrodd <tbrodd@kth.se>
2016-08-27 18:12:03 +0200

    made use of super variables PRICE and DESCRIPTION

--- dissen/signup/models/main_sponsor_package.py
+++ dissen/signup/models/main_sponsor_package.py
@@ -1,6 +1,5 @@
 class MainSponsorPackage(Package):
     """Describes the package for the main sponsor."""
 
-    def __init__(self):
-        self.PRICE = 120000
-        self.DESCRIPTION = ""
+    super().PRICE = 120000
+    super().DESCRIPTION = ""
