97afdca23c89432c14b50924f2572167c61b496b
tbrodd <tbrodd@kth.se>
2016-08-27 18:10:48 +0200

    made use of the super variable PRICE and changed so that certain fields are stored in the database

--- dissen/signup/models/additional_order_package.py
+++ dissen/signup/models/additional_order_package.py
@@ -6,18 +6,21 @@ class AdditionalOrderPackage():
     DRINK_TICKET_PRICE = 30
     TABLE_PRICE = 300
 
-    people_day = 0
-    people_evening = 0
-    drink_tickets = 0
-    tables = 0
+    people_day = models.IntegerField()
+    people_evening = models.IntegerField()
+    drink_tickets = models.IntegerField()
+    tables = models.IntegerField()
 
     def __init__(self, people_day, people_evening, drink_tickets, tables):
+        super().PRICE = self.__calculate_total_price()
+        super().DESCRIPTION = ""
+
         self.people_day = people_day
         self.people_evening = people_evening
         self.drink_tickets = drink_tickets
         self.tables = tables
 
-    def calculate_total_price(self):
+    def __calculate_total_price(self):
         return (people_day * PEOPLE_DAY_PRICE) +
         (people_evening * PEOPLE_EVENING_PRICE) +
         (drink_tickets * DRINK_TICKET_PRICE) +
