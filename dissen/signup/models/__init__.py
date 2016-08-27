42c763562aec59c94de249c9d436f774ca118f14
tbrodd <tbrodd@kth.se>
2016-08-27 17:29:40 +0200

    added the new models

--- dissen/signup/models/__init__.py
+++ dissen/signup/models/__init__.py
@@ -1,2 +1,6 @@
 # this file should import all models in this directory
 from contact import Contact
+from package import Package
+from base_package import BasePackage
+from main_sponsor_package import MainSponsorPackage
+from additional_order_package import AdditionalOrderPackage
