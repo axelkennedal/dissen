c4d24ed5f7747b82ecce3d8f0d7af9428271e0c0
Gustav Ung <guaschman@gmail.com>
2016-09-04 00:18:36 +0200

    Added the signup module to the project

--- dissen/dissen/settings.py
+++ dissen/dissen/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'signup.apps.SignupConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
