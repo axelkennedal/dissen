4e01133e8d7deac8365075270d1fdbc446974a73
kennedal <kennedal@kth.se>
2016-09-04 01:15:11 +0200

    fixed incorrect imports

--- dissen/signup/models/__init__.py
+++ dissen/signup/models/__init__.py
@@ -1,9 +1,2 @@
 # this file should import all models in this directory
-from contact import Contact
-from company import Company
-from event import Event
-
-from package import Package
-from base_package import BasePackage
-from main_sponsor_package import MainSponsorPackage
-from additional_order_package import AdditionalOrderPackage
+from . import AdditionalOrderPackage, BasePackage, Company, Contact, Event, MainSponsorPackage, Package
