65e3cccace36271493e6f84e798394d862837672
kennedal <kennedal@kth.se>
2016-09-04 01:21:22 +0200

    added company relation and string representation to Package

--- dissen/signup/models/package.py
+++ dissen/signup/models/package.py
@@ -3,7 +3,10 @@ from django.db import models
 class Package(models.Model):
     """Describes a package."""
 
+    def __str__(self):
+        return self.company.name
+
     PRICE = models.IntegerField()
     DESCRIPTION = models.CharField(max_length = 75)
     TIMESTAMP = models.DateTimeField(auto_now_add = True)
-    # todo: add reference to a company
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
