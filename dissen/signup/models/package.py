1423c05c08068c193ac10eedcb48ba644ea85f5a
kennedal <kennedal@kth.se>
2016-09-04 02:26:26 +0200

    wrote more descriptive class comment for Package, increased max length of DESCRIPTION

--- dissen/signup/models/package.py
+++ dissen/signup/models/package.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Package(models.Model):
-    """Describes a package."""
+    """A package is a set of services D-Dagen can provide to a company."""
 
     def __str__(self):
         return self.company.name
@@ -10,6 +10,6 @@ class Package(models.Model):
         abstract = True
 
     PRICE = models.IntegerField()
-    DESCRIPTION = models.CharField(max_length = 75)
+    DESCRIPTION = models.CharField(max_length = 200)
     TIMESTAMP = models.DateTimeField(auto_now_add = True)
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
