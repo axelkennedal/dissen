ed6c682e2bfb3fbaf8a0a475374b52fac7679e67
Gustav Ung <guaschman@gmail.com>
2016-09-14 22:47:37 +0200

    signup: Added company events to admin view

--- dissen/signup/admin.py
+++ dissen/signup/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 from signup.models.company import Company
+from signup.models.event import Event
 # Register your models here.
 
 admin.site.register(Company)
+admin.site.register(Event)
