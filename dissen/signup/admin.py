d8e77980f2bdcf02d7b6e539e27a975200cecebd
Gustav Ung <guaschman@gmail.com>
2016-09-14 22:52:38 +0200

    signup: added fair and contact person to admin view

--- dissen/signup/admin.py
+++ dissen/signup/admin.py
@@ -2,7 +2,11 @@ from django.contrib import admin
 
 from signup.models.company import Company
 from signup.models.event import Event
+from signup.models.contact import Contact
+from signup.models.fair import Fair
 # Register your models here.
 
 admin.site.register(Company)
 admin.site.register(Event)
+admin.site.register(Contact)
+admin.site.register(Fair)
--- dissen/signup/models/contact.py
+++ dissen/signup/models/contact.py
@@ -1,10 +1,11 @@
 from django.db import models
 
+from company import Company
 class Contact(models.Model):
     """Describes a contact from a company."""
 
     def __str__(self):
-        return self.first_name + self.last_name + " @ " + self.company.name
+        return self.first_name + ' ' + self.last_name + " @ " + self.company.name
 
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
     first_name = models.CharField(max_length = 75)
