c5a701126b85c323634add29d3cf903829e75c69
kennedal <kennedal@kth.se>
2016-09-16 17:39:59 +0200

    created url and dummy view for the signup form

--- dissen/signup/views.py
+++ dissen/signup/views.py
@@ -5,3 +5,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the signup index.")
+
+def signup_form(request):
+    return HttpResponse("This is where you actually sign up.")
